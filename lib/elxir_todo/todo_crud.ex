11e617e303debd2ea2004ccb3283e68bce7f9ffb
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-10-17 17:42:29 -0400

    Utilize put_in macro in TodoCrud

--- lib/elxir_todo/todo_crud.ex
+++ lib/elxir_todo/todo_crud.ex
@@ -18,10 +18,11 @@ defmodule ElixirTodo.TodoCrud do
         %{date: _date, title: _title} = entry
       ) do
     # Set the id for the entry being added.
-    new_entry = entry |> Map.put(:id, auto_id)
+    new_entry = put_in(entry[:id], auto_id)
 
     # Add the new entry to the collection and increment the `auto_id` field.
-    updated_collection = collection |> Map.put(auto_id, new_entry)
+    updated_collection = put_in(collection[auto_id], new_entry)
+
     %TodoCrud{instance | collection: updated_collection, auto_id: auto_id + 1}
   end
 
@@ -53,7 +54,8 @@ defmodule ElixirTodo.TodoCrud do
         %TodoCrud{collection: collection} = instance,
         entry_id,
         updater_fn \\ fn entry -> entry end
-      ) do
+      )
+      when is_number(entry_id) do
     case collection[entry_id] do
       nil ->
         instance
@@ -64,8 +66,8 @@ defmodule ElixirTodo.TodoCrud do
         # - The entry ID should be unchanged after updated.
         entry_id = entry.id
         updated_entry = %{id: ^entry_id} = updater_fn.(entry)
-        updated_collection = collection |> Map.put(updated_entry.id, updated_entry)
-        %TodoCrud{instance | collection: updated_collection}
+
+        %TodoCrud{instance | collection: put_in(collection[entry_id], updated_entry)}
     end
   end
 
@@ -75,7 +77,8 @@ defmodule ElixirTodo.TodoCrud do
   def delete_entry(
         %TodoCrud{collection: collection} = instance,
         id
-      ) when is_number(id)  do
+      )
+      when is_number(id) do
     {_poped_entry, updated_collection} = pop_in(collection[id])
     instance |> Map.put(:collection, updated_collection)
   end
