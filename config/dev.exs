73389872d3813041123bfb1d737ee1ae0d4eb4e0
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-11-16 08:43:03 -0500

    Change db_* directory to tmp

--- config/dev.exs
+++ config/dev.exs
@@ -1,4 +1,4 @@
 use Mix.Config
 
 config :elixir_todo, http_port: 3000
-config :elixir_todo, db_directory: "db_dev"
+config :elixir_todo, db_directory: "tmp/db_dev"
--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,4 @@
 use Mix.Config
 
 config :elixir_todo, http_port: 3001
-config :elixir_todo, db_directory: "db_test"
+config :elixir_todo, db_directory: "tmp/db_test"
